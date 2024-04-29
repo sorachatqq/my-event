dd7c1e86b7f27e17a1e66aa96c7d6c7dec504a58
Aom Siriwat <siriwat576@gmail.com>
2024-04-29 19:46:13 +0700

    feat: Add EventState class for managing events in the app

---
+++ lib/utils/state/event_state.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+import 'package:my_event_flutter/utils/models/model_event.dart';
+import 'package:my_event_flutter/utils/services/native_api_service.dart';
+
+class EventState extends GetxController {
+  final List<EventModel> events = [];
+
+  @override
+  void onInit() {
+    init();
+    super.onInit();
+  }
+
+  void init() async {
+    await load();
+  }
+
+  Future<void> load() async {
+    events.clear();
+    final response = await NativeApiService.get("events");
+    Map data = response;
+    List<EventModel> loadEvents = List<EventModel>.from(data['data'].map((x) => EventModel.fromJson(x)));
+    events.addAll(loadEvents);
+  }
+}
